04dc09e610e21adf7f442e5373d02a0fadb13bab
suzarm <suzarm8883@gmail.com>
2024-04-03 20:18:26 +0400

    added game rules

--- script.js
+++ script.js
@@ -2,10 +2,66 @@
 let symbol = "o";
 function move(row,cell){
     console.log(row,cell);
-    if( symbol==="o"){
-        symbol="x"
-    } else {
-        symbol="o"
+    if(!document.getElementById("start").rows[row].cells[cell].innerHTML ){
+        if( symbol==="o"){
+            symbol="x"
+        } else {
+            symbol="o"
+        }
+        document.getElementById("start").rows[row].cells[cell].innerHTML = symbol;
+        setTimeout(function(){
+            if(document.getElementById("start").rows[0].cells[0].innerHTML ==document.getElementById("start").rows[0].cells[1].innerHTML &&
+            document.getElementById("start").rows[0].cells[2].innerHTML ==document.getElementById("start").rows[0].cells[0].innerHTML && document.getElementById("start").rows[0].cells[0].innerHTML ){
+            return  alert('game over');
+           }
+           if(document.getElementById("start").rows[1].cells[0].innerHTML ==document.getElementById("start").rows[1].cells[1].innerHTML &&
+            document.getElementById("start").rows[1].cells[2].innerHTML ==document.getElementById("start").rows[1].cells[0].innerHTML && document.getElementById("start").rows[1].cells[0].innerHTML ){
+             return  alert('game over');
+           }
+           if(document.getElementById("start").rows[2].cells[0].innerHTML ==document.getElementById("start").rows[2].cells[1].innerHTML &&
+           document.getElementById("start").rows[2].cells[2].innerHTML ==document.getElementById("start").rows[2].cells[0].innerHTML && document.getElementById("start").rows[2].cells[0].innerHTML ){
+            return  alert('game over');
+          }
+          if(document.getElementById("start").rows[0].cells[0].innerHTML ==document.getElementById("start").rows[1].cells[0].innerHTML &&
+           document.getElementById("start").rows[2].cells[0].innerHTML ==document.getElementById("start").rows[0].cells[0].innerHTML && document.getElementById("start").rows[0].cells[0].innerHTML ){
+            return  alert('game over');
+          }
+          if(document.getElementById("start").rows[0].cells[1].innerHTML ==document.getElementById("start").rows[1].cells[1].innerHTML &&
+           document.getElementById("start").rows[2].cells[1].innerHTML ==document.getElementById("start").rows[0].cells[1].innerHTML && document.getElementById("start").rows[0].cells[1].innerHTML ){
+            return  alert('game over');
+          }
+          if(document.getElementById("start").rows[0].cells[1].innerHTML ==document.getElementById("start").rows[1].cells[1].innerHTML &&
+           document.getElementById("start").rows[2].cells[1].innerHTML ==document.getElementById("start").rows[0].cells[1].innerHTML && document.getElementById("start").rows[0].cells[1].innerHTML ){
+            return  alert('game over');
+          }
+          if(document.getElementById("start").rows[0].cells[2].innerHTML ==document.getElementById("start").rows[1].cells[2].innerHTML &&
+           document.getElementById("start").rows[2].cells[2].innerHTML ==document.getElementById("start").rows[0].cells[2].innerHTML && document.getElementById("start").rows[0].cells[2].innerHTML ){
+            return alert('game over');
+          }
+          if(document.getElementById("start").rows[0].cells[0].innerHTML ==document.getElementById("start").rows[1].cells[1].innerHTML &&
+           document.getElementById("start").rows[2].cells[2].innerHTML ==document.getElementById("start").rows[0].cells[0].innerHTML && document.getElementById("start").rows[0].cells[0].innerHTML ){
+            return alert('game over');
+          }
+          if(document.getElementById("start").rows[2].cells[0].innerHTML ==document.getElementById("start").rows[1].cells[1].innerHTML &&
+          document.getElementById("start").rows[0].cells[2].innerHTML ==document.getElementById("start").rows[2].cells[0].innerHTML && document.getElementById("start").rows[2].cells[0].innerHTML ){
+            return alert('game over');
+         }
+         if(document.getElementById("start").rows[0].cells[0].innerHTML &&
+          document.getElementById("start").rows[0].cells[1].innerHTML &&
+          document.getElementById("start").rows[0].cells[2].innerHTML &&
+          document.getElementById("start").rows[1].cells[0].innerHTML &&
+          document.getElementById("start").rows[1].cells[1].innerHTML &&
+          document.getElementById("start").rows[1].cells[2].innerHTML &&
+          document.getElementById("start").rows[2].cells[0].innerHTML &&
+          document.getElementById("start").rows[2].cells[1].innerHTML &&
+          document.getElementById("start").rows[2].cells[2].innerHTML){
+            return alert('draw');
+          }
+          
+         
+        },0)
+        
+        
     }
-    document.getElementById("start").rows[row].cells[cell].innerHTML = symbol;
+   
 }
